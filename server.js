458909fbe9cf7a92fb50769e0e5a87c63a7ad7e5
Abdu Samaraie <abdusamaraie@gmail.com>
2019-12-22 21:12:55 -0500

    Finish the create route

--- server.js
+++ server.js
@@ -18,15 +18,58 @@ var blogSchema = mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  create: { type: Date, defult: Date.now }
+  created: { type: Date, default: Date.now }
 });
 
 //Create modle
 var Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//   title: "test blog",
+//   image:
+//     "https://images.unsplash.com/photo-1554825203-68321ddde262?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   body: " this is a test blog with a simple image and text"
+// });
+
+//Restfull routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("/blogs");
 });
+//INDEX
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+//CREATE
+app.post("/blogs", (req, res) => {
+  //create a blog post
+  Blog.create(req.body.blog, (err, newPost) => {
+    if (err) {
+      res.render("new");
+    } else {
+      //redirect back to blogs
+      res.redirect("blogs");
+    }
+  });
+});
+//NEW
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
+});
+//SHOW
+app.get("/blogs/:id/show", (req, res) => {});
+//EDIT
+app.get("/blogs/:id/edit", (req, res) => {});
+//UPDATE
+app.put("/blogs/:id", (req, res) => {});
+//DELETE
+app.delete("/blogs/:id", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("server running on port 3000!!");
